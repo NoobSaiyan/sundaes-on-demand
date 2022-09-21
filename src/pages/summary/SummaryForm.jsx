583bcc7242058c1f23931f4d027254442a16f530
NoobSaiyan <ms19961998@gmail.com>
2022-09-21 20:11:26 +0530

    testing pop over

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Popover from 'react-bootstrap/Popover'
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
+
 export default function SummaryForm() {
   const [tcChecked, setTcChecked] = useState(false)
 
+  const popover = (
+    <Popover id="termsandconditions-popover">
+      No ice cream will actually be delivered
+    </Popover>
+  )
+
   const checkboxLabel = (
     <span>
-      I agree to <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      I agree to
+      <OverlayTrigger placement="right" overlay={popover}>
+        <span style={{ color: 'blue' }}> Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   )
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,8 @@
-import { render, screen } from '@testing-library/react'
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import SummaryForm from '../SummaryForm'
@@ -23,4 +27,27 @@ test('button is enabled on selecting check box', () => {
   // click check checkbox
   userEvent.click(checkBox)
   expect(confirmButton).toBeDisabled()
+})
+
+test('popover responds to hover', async () => {
+  render(<SummaryForm />)
+
+  // popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i,
+  )
+  expect(nullPopover).not.toBeInTheDocument()
+
+  // popover appearsupon mouseover of the checkbox label
+  const termsAndConditions = screen.getByText(/terms and conditions/i)
+  userEvent.hover(termsAndConditions)
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i)
+  expect(popover).toBeInTheDocument()
+
+  // popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions)
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i),
+  )
 })
